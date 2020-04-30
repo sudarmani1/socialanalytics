f7886c94dff89f99e93bbf7f46fee8f6feae0a97
D Ashwin <ashwinmathew33@gmail.com>
2020-04-30 22:46:41 +0530

    Added heroku files

--- apps/insta/urls.py
+++ apps/insta/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('insta-followers', views.insta_follower_list, name='insta-followers'),
     path('insta-following', views.insta_following_list, name='insta-following'),
     path('insta-update', views.update_insta_feed, name='insta-update'),
-    path('twilio', views.twilio, name='twilio'),
+    path('twilio/', views.twilio, name='twilio'),
     path('sendmail', views.sendmail, name='sendmail'),
 ]
