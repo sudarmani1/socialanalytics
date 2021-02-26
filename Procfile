ac07a3e1490adb45ddd81a7d15a3c99913d0e05b
D Ashwin <ashwinmathew33@gmail.com>
2021-02-27 02:00:41 +0530

    del

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn analytics.wsgi --log-file -
--- sonar-scanner.sh
+++
@@ -1,7 +0,0 @@
-sonar-scanner \
-  -Dsonar.projectKey=soCan \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login= \
-  -Dsonar.python.coverage.reportPaths=./coverage_report/coverage.xml \
-  -Dsonar.exclusions=manage.py,apps/fb/**,**/*.js/*,**/*.css
