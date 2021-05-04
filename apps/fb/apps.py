fa8fcc9dfd0d47097347400bf32be950354d5137
D Ashwin <ashwinmathew33@gmail.com>
2021-05-05 03:47:11 +0530

    test

--- apps/fb/apps.py
+++ apps/fb/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class FbConfig(AppConfig):
-    name = 'fb'
+    name = 'apps.fb'
--- apps/insta/apps.py
+++ apps/insta/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class InstaConfig(AppConfig):
-    name = 'insta'
+    name = 'apps.insta'
